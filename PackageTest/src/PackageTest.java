4ecf3afa42fa04541889a76f9bd5448660bbf19f
unknown <1539140390@qq.com>
2019-10-02 11:11:38 +0800

    第三次提交

---
+++ PackageTest/src/PackageTest.java
@@ -0,0 +1,13 @@
+import course.*;
+import teacher.*;
+import student.*;
+public class PackageTest {
+
+	public static void main(String[] args) {
+		Teacher te=new Teacher();
+		Student su=new Student();
+		Course cs=new Course();
+
+	}
+
+}
